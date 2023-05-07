53683c062d37afb8a3a2343544a902041dadbdd3
Bong-oh Moon <bongohmoon@gmail.com>
2023-05-07 15:38:52 +0900

    fix(client): Property 'crossorigin' does not exist on type - #13

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -17,7 +17,7 @@ export default function Document() {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
