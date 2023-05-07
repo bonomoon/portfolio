9fcbf3272e79568c8f7513bdc80e9f321366ec66
Bong-oh Moon <bongohmoon@gmail.com>
2023-05-07 15:08:45 +0900

    fix(client): add DM Sans font-weight: 500

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -18,10 +18,7 @@ export default function Document() {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
