ea8cf20ae6d9805b80488c40739b8945af42363b
Bong-oh Moon <bongohmoon@gmail.com>
2023-05-09 23:15:33 +0900

    feat(client): add footer component to Layout. - #14

---
+++ apps/web/src/components/organisms/Footer.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import Container from "@components/atoms/Container";
+
+/**
+ * Styled footer container component.
+ */
+const FooterContainer = styled.footer`
+  display: flex;
+  position: relative;
+  width: 100%;
+  max-width: 100%;
+  overflow: hidden;
+  padding: 0 0 2rem 0;
+`;
+
+/**
+ * Footer component.
+ */
+const Footer = () => (
+  <FooterContainer>
+    <Container>
+
+    </Container>
+  </FooterContainer>
+);
+export default Footer;
